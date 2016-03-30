47d5eaa32fa29c8dfdb9f6f375b2ad79cf67298d
Lawrence Macharia <macharialawrence2011@gmail.com>
2016-03-30 21:47:19 +0300

    You can inject something on the root component and all other child components will be able to use it

--- dev/boot.ts
+++ dev/boot.ts
@@ -1,5 +1,6 @@
 /// <reference path="../typings/browser.d.ts" />
 import {bootstrap} from 'angular2/platform/browser';
 import {AppComponent} from "./app.component";
+import {LoggingService} from "./services/logging.service";
 
-bootstrap(AppComponent);
+bootstrap(AppComponent,[LoggingService]);
