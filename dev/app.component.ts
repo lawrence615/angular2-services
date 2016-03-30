a46a0dc367a34a7307d334e92e71d40ebbf49072
Lawrence Macharia <macharialawrence2011@gmail.com>
2016-03-30 21:41:16 +0300

    included our component, component-1 and added it in our directives array

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,11 +1,13 @@
 import {Component} from 'angular2/core';
+import {Component1Component} from "./component1.component";
 
 @Component({
     selector: 'my-app',
     template: `
-        <h1>Angular 2 Boilerplate</h1>
-        <p>Hello World!</p>
+        <h1>Component 1</h1>
+        <component-1></component-1>
     `,
+    directives:[Component1Component]
 })
 export class AppComponent {
 
