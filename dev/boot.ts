e4d4e7faeff5b701cbc6723b32af3dc0aaa56b23
Lawrence Macharia <macharialawrence2011@gmail.com>
2016-03-31 05:09:33 +0300

    We removed the LoggingService from the parent component so that we can inject a different instance of it in the child components

--- dev/boot.ts
+++ dev/boot.ts
@@ -3,4 +3,4 @@ import {bootstrap} from 'angular2/platform/browser';
 import {AppComponent} from "./app.component";
 import {LoggingService} from "./services/logging.service";
 
-bootstrap(AppComponent,[LoggingService]);
+bootstrap(AppComponent);
