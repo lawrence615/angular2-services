07cacb163cb492d649359fd7eae993de52053a25
Lawrence Macharia <macharialawrence2011@gmail.com>
2016-03-31 05:11:16 +0300

    Keeping track of the new message and old message

--- dev/services/logging.service.ts
+++ dev/services/logging.service.ts
@@ -3,7 +3,10 @@ import {Injectable} from "angular2/core";
 
 export class LoggingService {
 
+    private _lastMessage = '';
+
     log(message:string) {
-        console.log(message);
+        console.log('Current message: ' + message + ' Last message:' + this._lastMessage);
+        this._lastMessage = message;
     }
 }
